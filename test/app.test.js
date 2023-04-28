cf59d5b06c448568b29ed7aa9831218953617664
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2023-04-28 17:02:25 -0300

    Must connect to http://localhost :3001 and answer status code 200

--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,11 @@
-describe('App', () => {
-  test('Mock test', () => {
-    const sut = 10
-    expect(sut).toBe(10)
-    expect(sut).not.toBeNull()
-  })
+const supertest = require('supertest')
+const request = supertest('http://localhost:3001')
 
-  it('Should work with objects', () => {
-    const sut = { name: 'Rodrigo', age: 36 }
-    const sut2 = { name: 'Rodrigo', age: 36 }
-    expect(sut).toHaveProperty('name')
-    expect(sut).toHaveProperty('name', 'Rodrigo')
-    expect(sut.name).toBe('Rodrigo')
-    expect(sut.age).toBe(36)
-    expect(sut).toStrictEqual(sut2)
+describe('App', () => {
+  // TODO server response test
+  describe('Server', () => {
+    it('Should respond on 3001 port', () => {
+      return request.get('/').then(res => expect(res.status).toBe(200))
+    })
   })
 })
