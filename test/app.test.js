e2c325d8eae3999d33b207a16b60a417375d213c
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2023-04-28 13:49:07 -0300

    Jest overview

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,17 @@
 describe('App', () => {
-  test('Mock test', () => {})
+  test('Mock test', () => {
+    const sut = 10
+    expect(sut).toBe(10)
+    expect(sut).not.toBeNull()
+  })
+
+  it('Should work with objects', () => {
+    const sut = { name: 'Rodrigo', age: 36 }
+    const sut2 = { name: 'Rodrigo', age: 36 }
+    expect(sut).toHaveProperty('name')
+    expect(sut).toHaveProperty('name', 'Rodrigo')
+    expect(sut.name).toBe('Rodrigo')
+    expect(sut.age).toBe(36)
+    expect(sut).toStrictEqual(sut2)
+  })
 })
